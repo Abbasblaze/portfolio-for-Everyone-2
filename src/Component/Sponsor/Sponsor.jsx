0900e6986e9d432bde8737400fca42e67663a73e
Abbasblaze <abbas.raazaa40@gmail.com>
2024-09-19 14:42:55 +0500

    change

---
+++ src/Component/Sponsor/Sponsor.jsx
@@ -0,0 +1,44 @@
+import './Sponsor.modules.css'
+
+function Sponsor() {
+    return ( 
+        <>
+        <div className="container">
+            <div className="Sponsor">
+            <div className="SponsorHeading">
+                <p>Creating stunning designs that make a real impact</p>
+                <div className="SponsorImg">
+                    <img src="Img/64243e11b21cc613f15f811f_Vector (4) (1) 1.png" alt="" />
+                    <img src="Img/Capture d’écran 2022-11-14 à 21.48 1.png" alt="" />
+                    <img src="Img/Clip path group.png" alt="" />
+                    <img src="Img/Group 1.png" alt="" />
+                    <img src="Img/Group 27.png" alt="" />
+                    <img src="Img/Group 362.png" alt="" />
+                    <img src="Img/Group 48095555.png" alt="" />
+                    <img src="Img/Group 48095878.png" alt="" />
+                    <img src="Img/Group.png" alt="" />
+                    <img src="Img/header-appaloosa-logo-1 2.png" alt="" />
+                    <img src="Img/image 3.png" alt="" />
+                    <img src="Img/image 14.png" alt="" />
+                    <img src="Img/Logo (1).png" alt="" />
+                    <img src="Img/logo rose nom 1.png" alt="" />
+                    <img src="Img/Logo.png" alt="" />
+                    <img src="Img/Molotov 2.png" alt="" />
+                    <img src="Img/Group 27.png" alt="" />
+                    <img src="Img/Group 362.png" alt="" />
+                    <img src="Img/64243e11b21cc613f15f811f_Vector (4) (1) 1.png" alt="" />
+                    <img src="Img/Capture d’écran 2022-11-14 à 21.48 1.png" alt="" />
+                    <img src="Img/Clip path group.png" alt="" />
+        
+    
+          
+                    
+                </div>
+            </div>
+            </div>
+        </div>
+        </>
+     );
+}
+
+export default Sponsor;
